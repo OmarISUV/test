998e25f4f0bbb9eefbe62c166785d06336383251
Francisco Ramos <fro.profesional@gmail.com>
2023-01-16 11:44:41 -0800

    Add prisma

---
+++ src/config/prisma.mjs
@@ -0,0 +1,9 @@
+/**
+ * npm i @prisma/client
+ * import{ client as PrismaClient } from "../config/prisma.mjs"
+ */
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+export { client };
--- src/routes/hello.mjs
+++ src/routes/hello.mjs
@@ -1,16 +1,30 @@
 import { Router } from "express";
 import cors from "cors";
 import { middleware as HelloMiddleware } from "../middleware/hello.mjs";
-import { client as MongoClient } from "../config/mongodb.mjs"
+import { client as PrismaClient } from "../config/prisma.mjs";
 
 const route = Router();
 
 route.use(cors());
 
 route.get("/", HelloMiddleware, async (request, response) => {
-  await MongoClient.connect()
-  
-  response.send("Hello World!!-----");
+  await PrismaClient.$connect();
+
+  await PrismaClient.user.create({
+    data: {
+      name: "Rich",
+      email: "hello@prisma.com",
+      posts: {
+        create: {
+          title: "My first post",
+          body: "Lots of really interesting stuff",
+          slug: "my-first-post",
+        },
+      },
+    },
+  });
+
+  response.send("Hello World!!");
 });
 
 export { route };
